cdd1ba19c05173226b9740439f8b7d925d7bd8e2
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-11-14 20:33:57 -0600

    cards and images showing

--- javascripts/controllers/SearchCtrl.js
+++ javascripts/controllers/SearchCtrl.js
@@ -1,10 +1,12 @@
 "use strict";
 
 app.controller("SearchCtrl", function($scope, tmdbService){
+	$scope.movies = [];
+
 	$scope.enterPush = (event) => {
 		if(event.keyCode === 13) {
-			console.log("event", event.target.value);
 			tmdbService.searchMovies(event.target.value).then((results) =>{
+				$scope.movies = results.data.results;
 			console.log("movies?", results.data.results);
 		}).catch((err) => {
 			console.log("error in searchMovies", err);
