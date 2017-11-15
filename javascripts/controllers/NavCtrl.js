01249784c5c39ede090b36b717119e4768acf132
Caitlin Murnick <caitlinmurnick@gmail.com>
2017-11-14 21:07:22 -0600

    logout works

---
+++ javascripts/controllers/NavCtrl.js
@@ -0,0 +1,10 @@
+"use strict";
+
+app.controller("NavCtrl", function ($location, $rootScope, $scope, $window) {
+	$scope.logoutUser = () => {
+		delete $rootScope.uid;
+		$window.localStorage.clear();
+		$location.path('/auth')
+
+	};
+});
